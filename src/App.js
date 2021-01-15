0984f720ccc8ff383658b35afbe4805d9803888f
mathieutranchida <mathieu.tranchida@gmail.com>
2021-01-15 15:46:15 -0500

    app setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-function App() {
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+import GlobalStyles from "./GlobalStyles";
+import Home from "./components/index";
+
+const App = () => {
   return (
     <>
-      <div className="App">Hello world</div>
+      <BrowserRouter>
+        <GlobalStyles />
+        <div>
+          <Switch>
+            <Route exact patch="/" component={Home} />
+          </Switch>
+        </div>
+      </BrowserRouter>
     </>
   );
-}
+};
 
 export default App;
---
+++ src/components/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const Home = () => {
+  return (
+    <>
+      <Wrapper>Hello world</Wrapper>
+    </>
+  );
+};
+
+const Wrapper = styled.div``;
+
+export default Home;
