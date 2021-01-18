fa525f5fed9ae0b9abe078ea65f3ea0101808050
mathieutranchida <mathieu.tranchida@gmail.com>
2021-01-18 13:24:06 -0500

    scroll reveal feature added

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,7 @@
-import React, { useRef } from "react";
+import React, { useRef, useEffect } from "react";
 import styled from "styled-components";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 import TopHeader from "./header/topHeader/TopHeader";
 import ScrollHeader from "./header/scrollHeader/ScrollHeader";
@@ -12,6 +14,35 @@ import WorkExperience from "./workExperience/WorkExperience";
 import Contact from "./contact/Contact";
 
 const Index = () => {
+  let revealRefs = useRef([]);
+  revealRefs.current = [];
+
+  const addToRefs = (el) => {
+    if (el && !revealRefs.current.includes(el)) {
+      revealRefs.current.push(el);
+    }
+  };
+
+  useEffect(() => {
+    revealRefs.current.forEach((el, index) => {
+      gsap.fromTo(
+        el,
+        { opacity: 0 },
+        {
+          duration: 1.5,
+          opacity: 1,
+          ease: "power6.out",
+          scrollTrigger: {
+            id: `section-${index + 1}`,
+            trigger: el,
+            start: "top bottom-=150",
+            toggleActions: "play none none none",
+          },
+        }
+      );
+    });
+  }, []);
+
   return (
     <>
       <Wrapper>
@@ -19,12 +50,24 @@ const Index = () => {
         <ScrollHeader />
         <IntroBackground />
         <Main>
-          <Bio />
-          <Education />
-          <Skills />
-          <Projects />
-          <WorkExperience />
-          <Contact />
+          <section id="section1" ref={addToRefs}>
+            <Bio />
+          </section>
+          <section id="section2" ref={addToRefs}>
+            <Education />
+          </section>
+          <section id="section3" ref={addToRefs}>
+            <Skills />
+          </section>
+          <section id="section4" ref={addToRefs}>
+            <Projects />
+          </section>
+          <section id="section5" ref={addToRefs}>
+            <WorkExperience />
+          </section>
+          <section id="section6" ref={addToRefs}>
+            <Contact />
+          </section>
         </Main>
       </Wrapper>
     </>
--- src/components/introBackground/index.js
+++ src/components/introBackground/index.js
@@ -4,7 +4,7 @@ import styled from "styled-components";
 const Index = () => {
   return (
     <>
-      <Wrapper></Wrapper>
+      <Wrapper />
     </>
   );
 };
