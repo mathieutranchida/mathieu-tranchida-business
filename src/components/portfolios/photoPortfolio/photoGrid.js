a265cf51f1690548c0e29283d6975fb993a8aa6d
mathieutranchida <mathieu.tranchida@gmail.com>
2021-01-19 00:58:54 -0500

    desktop photo gallery

--- src/components/portfolios/photoPortfolio/photoGrid.js
+++ src/components/portfolios/photoPortfolio/photoGrid.js
@@ -4,11 +4,284 @@ import styled from "styled-components";
 const PhotoGrid = () => {
   return (
     <>
-      <Wrapper></Wrapper>
+      <Wrapper>
+        <Grid>
+          <GridItem1>
+            <Image
+              src="https://res.cloudinary.com/dldqebddc/image/upload/v1611030694/Business%20website/Grid%20Photo/photoGrid-31_pnw9hz.jpg"
+              alt="gallery photo image"
+            />
+          </GridItem1>
+          <GridItem2>
+            <Image
+              src="https://res.cloudinary.com/dldqebddc/image/upload/v1611030693/Business%20website/Grid%20Photo/photoGrid-13_xdts27.jpg"
+              alt="gallery photo image"
+            />
+          </GridItem2>
+          <GridItem3>
+            <Image
+              src="https://res.cloudinary.com/dldqebddc/image/upload/v1611030693/Business%20website/Grid%20Photo/photoGrid-23_l7mm6i.jpg"
+              alt="gallery photo image"
+            />
+          </GridItem3>
+          <GridItem4>
+            <Image
+              src="https://res.cloudinary.com/dldqebddc/image/upload/v1611030694/Business%20website/Grid%20Photo/photoGrid_vuvis6.jpg"
+              alt="gallery photo image"
+            />
+          </GridItem4>
+          <GridItem5>
+            <Image
+              src="https://res.cloudinary.com/dldqebddc/image/upload/v1611030688/Business%20website/Grid%20Photo/photoGrid-5_sy8wvh.jpg"
+              alt="gallery photo image"
+            />
+          </GridItem5>
+          <GridItem6>
+            <Image
+              src="https://res.cloudinary.com/dldqebddc/image/upload/v1611030694/Business%20website/Grid%20Photo/photoGrid-25_s40vww.jpg"
+              alt="gallery photo image"
+            />
+          </GridItem6>
+          <GridItem7>
+            <Image
+              src="https://res.cloudinary.com/dldqebddc/image/upload/v1611030688/Business%20website/Grid%20Photo/photoGrid-2_nuogki.jpg"
+              alt="gallery photo image"
+            />
+          </GridItem7>
+          <GridItem8>
+            <Image
+              src="https://res.cloudinary.com/dldqebddc/image/upload/v1611030693/Business%20website/Grid%20Photo/photoGrid-28_jj09hj.jpg"
+              alt="gallery photo image"
+            />
+          </GridItem8>
+          <GridItem9>
+            <Image
+              src="https://res.cloudinary.com/dldqebddc/image/upload/v1611030693/Business%20website/Grid%20Photo/photoGrid-24_yec3ut.jpg"
+              alt="gallery photo image"
+            />
+          </GridItem9>
+          <GridItem10>
+            <Image
+              src="https://res.cloudinary.com/dldqebddc/image/upload/v1611030691/Business%20website/Grid%20Photo/photoGrid-16_hfwdpy.jpg"
+              alt="gallery photo image"
+            />
+          </GridItem10>
+          <GridItem11>
+            <Image
+              src="https://res.cloudinary.com/dldqebddc/image/upload/v1611030691/Business%20website/Grid%20Photo/photoGrid-15_bl6ayu.jpg"
+              alt="gallery photo image"
+            />
+          </GridItem11>
+          <GridItem12>
+            <Image
+              src="https://res.cloudinary.com/dldqebddc/image/upload/v1611030689/Business%20website/Grid%20Photo/photoGrid-8_ykaatd.jpg"
+              alt="gallery photo image"
+            />
+          </GridItem12>
+          <GridItem13>
+            <Image
+              src="https://res.cloudinary.com/dldqebddc/image/upload/v1611030693/Business%20website/Grid%20Photo/photoGrid-26_hfh2mn.jpg"
+              alt="gallery photo image"
+            />
+          </GridItem13>
+          <GridItem14>
+            <Image
+              src="https://res.cloudinary.com/dldqebddc/image/upload/v1611030688/Business%20website/Grid%20Photo/photoGrid-7_f0hrgi.jpg"
+              alt="gallery photo image"
+            />
+          </GridItem14>
+          <GridItem15>
+            <Image
+              src="https://res.cloudinary.com/dldqebddc/image/upload/v1611030689/Business%20website/Grid%20Photo/photoGrid-11_abqnxh.jpg"
+              alt="gallery photo image"
+            />
+          </GridItem15>
+          <GridItem16>
+            <Image
+              src="https://res.cloudinary.com/dldqebddc/image/upload/v1611030688/Business%20website/Grid%20Photo/photoGrid-3_tx4utx.jpg"
+              alt="gallery photo image"
+            />
+          </GridItem16>
+          <GridItem17>
+            <ImageCentered
+              src="https://res.cloudinary.com/dldqebddc/image/upload/v1611030692/Business%20website/Grid%20Photo/photoGrid-19_npfj2p.jpg"
+              alt="gallery photo image"
+            />
+          </GridItem17>
+          <GridItem18>
+            <ImageCentered
+              src="https://res.cloudinary.com/dldqebddc/image/upload/v1611030694/Business%20website/Grid%20Photo/photoGrid-29_bhyerx.jpg"
+              alt="gallery photo image"
+            />
+          </GridItem18>
+        </Grid>
+        <GridSmartphone>
+          <ImageSmartphone
+            src="https://res.cloudinary.com/dldqebddc/image/upload/v1611030694/Business%20website/Grid%20Photo/photoGrid-29_bhyerx.jpg"
+            alt="gallery photo image"
+          />
+          <ImageSmartphone
+            src="https://res.cloudinary.com/dldqebddc/image/upload/v1611030691/Business%20website/Grid%20Photo/photoGrid-16_hfwdpy.jpg"
+            alt="gallery photo image"
+          />
+          <ImageSmartphone
+            src="https://res.cloudinary.com/dldqebddc/image/upload/v1611030694/Business%20website/Grid%20Photo/photoGrid-31_pnw9hz.jpg"
+            alt="gallery photo image"
+          />
+        </GridSmartphone>
+      </Wrapper>
     </>
   );
 };
 
 const Wrapper = styled.div``;
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(6, 1fr);
+  grid-template-rows: repeat(29, 2.5vw);
+  grid-gap: 15px;
+  padding: 15px;
+  @media (max-width: 2500px) {
+    grid-template-rows: repeat(29, 2.2vw);
+  }
+  @media (max-width: 1750px) {
+    grid-template-rows: repeat(29, 2vw);
+  }
+  @media (max-width: 1250px) {
+    grid-template-rows: repeat(29, 1.8vw);
+  }
+  @media (max-width: 1100px) {
+    grid-template-rows: repeat(29, 1.5vw);
+  }
+  @media (max-width: 950px) {
+    grid-template-rows: repeat(29, 1.3vw);
+  }
+  @media (max-width: 850px) {
+    grid-template-rows: repeat(29, 1.1vw);
+  }
+  @media (max-width: 800px) {
+    display: none;
+  }
+`;
+
+const GridSmartphone = styled.div`
+  padding: 1.25vw 0px;
+  @media (min-width: 801px) {
+    display: none;
+  }
+`;
+
+const Image = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  display: block;
+  object-position: bottom;
+  user-select: none;
+`;
+
+const ImageCentered = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  display: block;
+  user-select: none;
+`;
+
+const GridItem1 = styled.div`
+  grid-column: 1/2;
+  grid-row: 1/8;
+`;
+
+const GridItem2 = styled.div`
+  grid-column: 2/4;
+  grid-row: 1/8;
+`;
+
+const GridItem3 = styled.div`
+  grid-column: 4/5;
+  grid-row: 1/9;
+`;
+
+const GridItem4 = styled.div`
+  grid-column: 5/6;
+  grid-row: 1/5;
+`;
+
+const GridItem5 = styled.div`
+  grid-column: 5/6;
+  grid-row: 5/9;
+`;
+
+const GridItem6 = styled.div`
+  grid-column: 6/7;
+  grid-row: 1/9;
+`;
+
+const GridItem7 = styled.div`
+  grid-column: 1/3;
+  grid-row: 8/15;
+`;
+
+const GridItem8 = styled.div`
+  grid-column: 3/4;
+  grid-row: 8/15;
+`;
+
+const GridItem9 = styled.div`
+  grid-column: 4/5;
+  grid-row: 9/17;
+`;
+
+const GridItem10 = styled.div`
+  grid-column: 5/7;
+  grid-row: 9/17;
+`;
+
+const GridItem11 = styled.div`
+  grid-column: 1/2;
+  grid-row: 15/23;
+`;
+
+const GridItem12 = styled.div`
+  grid-column: 2/3;
+  grid-row: 15/23;
+`;
+
+const GridItem13 = styled.div`
+  grid-column: 3/4;
+  grid-row: 15/23;
+`;
+
+const GridItem14 = styled.div`
+  grid-column: 4/6;
+  grid-row: 17/23;
+`;
+
+const GridItem15 = styled.div`
+  grid-column: 6/7;
+  grid-row: 17/23;
+`;
+
+const GridItem16 = styled.div`
+  grid-column: 1/3;
+  grid-row: 23/30;
+`;
+
+const GridItem17 = styled.div`
+  grid-column: 3/5;
+  grid-row: 23/30;
+`;
+
+const GridItem18 = styled.div`
+  grid-column: 5/7;
+  grid-row: 23/30;
+`;
+
+const ImageSmartphone = styled.img`
+  max-width: 95vw;
+  padding: 1.25vw 2.5vw;
+`;
 
 export default PhotoGrid;
