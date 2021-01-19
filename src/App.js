310a7206fa20ea30f4183ed2c7793211305946ac
mathieutranchida <mathieu.tranchida@gmail.com>
2021-01-19 12:39:44 -0500

    fixed scroll to top bug

--- src/App.js
+++ src/App.js
@@ -5,19 +5,26 @@ import GlobalStyles from "./GlobalStyles";
 import Home from "./components/resume/index";
 import PhotoPortfolio from "./components/portfolios/photoPortfolio/index";
 import DesignPortfolio from "./components/portfolios/designPortfolio/index";
+import ScrollToTop from "./scrollToTop";
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
-        <GlobalStyles />
-        <div>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/portfolio-photo" component={PhotoPortfolio} />
-            <Route exact path="/portfolio-design" component={DesignPortfolio} />
-          </Switch>
-        </div>
+        <ScrollToTop>
+          <GlobalStyles />
+          <div>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/portfolio-photo" component={PhotoPortfolio} />
+              <Route
+                exact
+                path="/portfolio-design"
+                component={DesignPortfolio}
+              />
+            </Switch>
+          </div>
+        </ScrollToTop>
       </BrowserRouter>
     </>
   );
